8866d6f300d8b5f4a34eaf271b54c6f9f48e6793
Joe <joe.mcnett@sundog.net>
2018-09-19 07:54:23 -0500

    Initial Commit

---
+++ demo/README.md
@@ -0,0 +1,2 @@
+# DF18-Let-CMS-Connect-Save-You-Time-and-Money
+Sample code from Dreamforce 18 presentation
---
+++ demo/cms-connect-demo/Pipelines/CORS.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Sitecore.Pipelines;
+using System.Web.Http.Cors;
+
+namespace cms_connect_demo.Pipelines
+{
+    public class CORS
+    {
+        public void Process(PipelineArgs args)
+        {
+            //GlobalConfiguration.Configuration.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+        }
+    }
+}
